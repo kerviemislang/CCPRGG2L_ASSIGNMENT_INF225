e3beefffd91ff2cbe7349a13da37df9284b79194
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:51:07 +0800

    Create Pet.java

---
+++ Exercise_6/Pet.java
@@ -0,0 +1,13 @@
+package exercise6;
+
+abstract class Pet {
+
+    // abstract method 
+    public abstract void eat();
+
+    // regular method 
+    public void sleep () {
+        System.out.println("ZZzzzzz");
+    }
+    
+}
