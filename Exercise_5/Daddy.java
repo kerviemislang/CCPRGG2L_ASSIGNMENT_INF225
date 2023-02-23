9b8acf3afc3c20b017c14e464f0e6ebaa8297db4
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:30:07 +0800

    Create Daddy.java

---
+++ Exercise_5/Daddy.java
@@ -0,0 +1,11 @@
+package exercise5;
+
+public class daddy {
+
+    protected static String surname = "Mislang";
+
+    public void showLove () {
+        System.out.println("I so much love " + surname + " family");
+    }
+
+}
