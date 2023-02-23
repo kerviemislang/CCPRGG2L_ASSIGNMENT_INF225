dda7651d359e31bd7a6f161b9b2f5669b80712fd
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:50:45 +0800

    Create Grocery Item.java

---
+++ Exercise_6/Grocery Item.java
@@ -0,0 +1,16 @@
+package exercise6;
+
+public class groceryItem {
+    
+    String name; 
+    double price; 
+
+    public void showItemName() {
+        System.out.println(this.name);
+    }
+
+    public void showPrice() {
+        System.out.println(this.price);
+     }
+    
+}
