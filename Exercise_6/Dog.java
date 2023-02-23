fc52c6a15985cbcdf793c6bfe7d485d29d0c27ee
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:50:15 +0800

    Create Dog.java

---
+++ Exercise_6/Dog.java
@@ -0,0 +1,10 @@
+package exercise6;
+
+public class dog extends Pet {
+
+    // implementing abstract method
+    public void eat() {
+        System.out.println("My dog is eating");
+    }
+    
+}
