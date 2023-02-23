39ae5b923050a1dc9606291d38cbd097749d30d0
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:06:19 +0800

    Create Character.java

---
+++ exercise3/Character.java
@@ -0,0 +1,19 @@
+package kervie;
+
+public class character {
+
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hi I am " + name);
+    }
+
+    public void attack() {
+        System.out.println("I am going to attack you!");
+
+    }
+
+}
