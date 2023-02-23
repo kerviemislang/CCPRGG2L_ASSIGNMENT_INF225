490215897aad4f8643ce0c2f168875c72f4e2afd
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:51:35 +0800

    Create Toothbrush.java

---
+++ Exercise_6/Toothbrush.java
@@ -0,0 +1,9 @@
+package exercise6;
+
+public class toothbrush extends groceryItem {
+    
+    public void showItemName() {
+        System.out.println(this.name + " is a toothbrush");
+    }
+    
+}
