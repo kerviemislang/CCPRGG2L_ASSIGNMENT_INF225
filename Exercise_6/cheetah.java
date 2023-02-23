cf33c8049bc25c4c1bac619d319e142969392992
Kervsss <122065497+kerviemislang@users.noreply.github.com>
2023-02-23 12:49:54 +0800

    Create cheetah.java

---
+++ Exercise_6/cheetah.java
@@ -0,0 +1,13 @@
+package exercise6;
+
+public class cheetah implements Animal{
+
+    public void walk() {
+        System.out.println("The Cheetah is walking");
+    }
+
+    public void run() {
+        System.out.println("The Cheetah is running");
+    }
+    
+}
